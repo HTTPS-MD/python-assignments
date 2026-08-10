56b00da30b4e173122a04f5f15868d3234481d17
Mark Daniel Francisco <franciscomarkdaniel850@gmail.com>
2026-08-10 16:41:51 +0800

    Anagram Checker

---
+++ Anagram-Checker /anagram-checker.py
@@ -0,0 +1,15 @@
+def is_anagram(w1 : str, w2 : str) -> bool:
+
+    w1 = "".join(sorted(w1.lower().replace(" ", "")))
+    w2 = "".join(sorted(w2.lower().replace(" ", "")))
+
+    if w1 == w2:
+        return True
+
+    return False
+
+if __name__ == "__main__":
+    first_word = input("Enter First Word: ")
+    second_word = input("Enter Second Word: ")
+
+    print(is_anagram(first_word, second_word))
