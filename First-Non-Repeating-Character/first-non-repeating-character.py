a692c42a89dec6e6d4b30d1b67a505a47f775596
Mark Daniel Francisco <franciscomarkdaniel850@gmail.com>
2026-08-11 05:15:17 +0800

    First Non Repeating Character Update

--- First-Non-Repeating-Character/first-non-repeating-character.py
+++ First-Non-Repeating-Character/first-non-repeating-character.py
@@ -1,14 +1,14 @@
-def first_non_repeating_character(word : str) -> chr:
-
+def first_non_repeating_character(text : str) -> str :
     dictionary = {}
 
-    for character in word:
+    for character in text:
         dictionary[character] = dictionary.get(character, 0) + 1
 
-    for letter in word:
-        if dictionary[letter] == 1:
-            return letter
+    for item in dictionary:
+        if dictionary[item] == 1:
+            return item
 
+    return ""
 
 if __name__ == "__main__":
     word = input("Enter a word: ")
