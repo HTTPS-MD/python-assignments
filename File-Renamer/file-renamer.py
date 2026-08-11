41055ed62f4ff092702058108179a8adf9473da2
Mark Daniel Francisco <franciscomarkdaniel850@gmail.com>
2026-08-12 05:01:02 +0800

    Updated File Renamer

--- File-Renamer/file-renamer.py
+++ File-Renamer/file-renamer.py
@@ -1,7 +1,14 @@
 file_name = input("Enter File Name: ")
-number = int(input("0 = Directory\n1 = Python File\nEnter Choice: "))
+
+number = -1
 special_symbols = '!@#$%^&*()_+={[}]|:;"<,>.?/ '
 
+print("\n0 = Directory\n1 = Python File\n2 = Function Name\n")
+while number < 0 or number > 2:
+    number = int(input("Enter Choice: "))
+
+    if number < 0 or number > 2:
+        print(" > Should not be > 2 or < 0!\n")
 
 
 new_name = ""
@@ -11,9 +18,13 @@ for character in file_name:
     else:
         new_name += "-"
 
+
 if number == 0:
     print(new_name.title())
 
-else:
+elif number == 1:
     print(f"{new_name.lower()}.py")
+
+elif number == 2:
+    print(f"{new_name.replace('-', '_')}")
 
