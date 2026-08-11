f5f38dac81bc4116326b1ecdaa6026fedf840c1d
Mark Daniel Francisco <franciscomarkdaniel850@gmail.com>
2026-08-11 17:56:10 +0800

    Updated

--- File-Renamer/file-renamer.py
+++ File-Renamer/file-renamer.py
@@ -1,10 +1,19 @@
 file_name = input("Enter File Name: ")
 number = int(input("0 = Directory\n1 = Python File\nEnter Choice: "))
+special_symbols = '!@#$%^&*()_+={[}]|:;"<,>.?/ '
 
 
+
+new_name = ""
+for character in file_name:
+    if character not in special_symbols:
+        new_name += character
+    else:
+        new_name += "-"
 
 if number == 0:
-    print(file_name.replace(" ", "-").title())
+    print(new_name.title())
 
 else:
-    print(file_name.replace(" ", "-").lower()+".py")
+    print(f"{new_name.lower()}.py")
+
