6e8da2fb81248bdaafd1d060b8892cd91160a7be
Mark Daniel Francisco <franciscomarkdaniel850@gmail.com>
2026-08-19 17:14:53 +0800

    Count Vowels

--- Count_Vowels/count_vowels.py
+++ Count_Vowels/count_vowels.py
@@ -3,5 +3,24 @@ def count_vowels(text : str) -> int :
 
     return sum(1 for char in text.lower() if char in "aeiou")
 
+
+def count_vowels_v2(text : str) -> int:
+
+    dictionary = {}
+    vowel_count = 0
+    vowels = "aeiou"
+
+    for char in text.lower():
+        dictionary[char] = dictionary.get(char, 0) + 1
+
+
+    for key, value in dictionary.items():
+        if key in vowels:
+            vowel_count += value
+
+    return vowel_count
+
+
 if __name__ == "__main__":
     print(count_vowels("Software Engineering"))  # Expected output: 8
+    print(count_vowels_v2("Software Engineering"))
