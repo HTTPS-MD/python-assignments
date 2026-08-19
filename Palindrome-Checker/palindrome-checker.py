49e449cfbee67d58929805439e4533c9ae1238b1
Mark Daniel Francisco <franciscomarkdaniel850@gmail.com>
2026-08-19 17:34:28 +0800

    Updated with 3 Versions

--- Palindrome-Checker/palindrome-checker.py
+++ Palindrome-Checker/palindrome-checker.py
@@ -15,8 +15,14 @@ def is_palindrome_v2(word: str) -> bool:
     return clean_sentence == clean_sentence[::-1]
 
 
+def is_palindrome_v3(word: str) -> bool:
+    clean = "".join(char for char in word.lower() if char.isalnum())
+
+    return clean[::-1] == clean
+
 if __name__ == "__main__":
 
-    print(is_palindrome_v2("Racecar")) # True
     print(is_palindrome("Python")) # False
+    print(is_palindrome_v2("Racecar"))  # True
+    print(is_palindrome_v3("Mark::::';k;raM"))  # True
 
