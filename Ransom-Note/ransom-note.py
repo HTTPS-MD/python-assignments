8a2cd3d540876e1d3094a4c48efcdf65dd6a3542
Mark Daniel Francisco <franciscomarkdaniel850@gmail.com>
2026-08-20 22:05:08 +0800

    Ransom Note with 2 version and fixes

--- Ransom-Note/ransom-note.py
+++ Ransom-Note/ransom-note.py
@@ -1,17 +1,26 @@
 from collections import Counter
 
-def can_construct(ransomNote: str, magazine: str) -> bool:
+def can_construct_v1(ransom_note: str, magazine: str) -> bool:
 
-    ransomNote = Counter(ransomNote)
     magazine = Counter(magazine)
+    ransom_note = Counter(ransom_note)
 
-    for key, value in ransomNote.items():
-        if magazine[key] < value:
+    for character in ransom_note:
+        if ransom_note[character] > magazine[character]:
             return False
 
     return True
 
+def can_construct_v2(ransom_note: str, magazine: str) -> bool:
+
+    ransom_note = Counter(ransom_note)
+    magazine = Counter(magazine)
+
+    for key, value in ransom_note.items():
+        if magazine[key] < value:
+            return False
+    return True
 if __name__ == "__main__":
-    print(can_construct("a", "b"))
-    print(can_construct("aa", "ab"))
-    print(can_construct("aa", "aab"))
+    print(can_construct_v1("a", "b"))
+    print(can_construct_v2("aa", "ab"))
+    print(can_construct_v2("aa", "aab"))
