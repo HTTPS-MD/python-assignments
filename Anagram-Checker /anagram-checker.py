3d4174a995382ded6ae64658dc789339c8cc8d68
Mark Daniel Francisco <franciscomarkdaniel850@gmail.com>
2026-08-20 21:38:13 +0800

    Anagram Checker 2 Version Update

--- Anagram-Checker /anagram-checker.py
+++ Anagram-Checker /anagram-checker.py
@@ -1,4 +1,11 @@
-def is_anagram(w1 : str, w2 : str) -> bool:
+from collections import Counter
+def is_anagram_v1(w1: str, w2: str) -> bool:
+    w1 = Counter(char.lower() for char in w1 if char.isalnum())
+    w2 = Counter(char.lower() for char in w2 if char.isalnum())
+    return w1 == w2
+
+
+def is_anagram_v2(w1 : str, w2 : str) -> bool:
 
     w1 = "".join(sorted(w1.lower().replace(" ", "")))
     w2 = "".join(sorted(w2.lower().replace(" ", "")))
@@ -9,4 +16,5 @@ if __name__ == "__main__":
     first_word = input("Enter First Word: ")
     second_word = input("Enter Second Word: ")
 
-    print(is_anagram(first_word, second_word))
+    print(is_anagram_v1(first_word, second_word))
+    print(is_anagram_v2(first_word, second_word))
